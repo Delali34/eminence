7ef8849afbacc07049adf2a85031c944ce0fc1a9
Dela <ernestanyomitse752@gmail.com>
2023-11-10 08:26:32 +0000

    eminencelead

--- components/Volunteers.js
+++ components/Volunteers.js
@@ -106,12 +106,12 @@ function Page() {
     <div className="bg5">
       <Navbar2 />
       <Mobilebar />
-      <section className="flex flex-col gap-10 justify-between items-center px-5 font-mont container mx-auto lg:px-10 py-10">
+      <section className="flex flex-col gap-10 justify-between items-center px-5 font-mont container mx-auto lg:px-10 py-5">
         <div className="lg:w-[80%]">
           <h1 className="text-white text-center text-4xl lg:text-6xl font-bold font-mont mt-5 text-shadow-black">
             Volunteer to support our vision
           </h1>
-          <p className="text-white text-center lg:text-2xl mb-10 text-[16px] font-mont mt-10">
+          <p className="text-white text-center lg:text-2xl lg:mb-10 mb-5 text-[16px] font-mont mt-10">
             Your time and skills are invaluable
           </p>
         </div>
@@ -123,7 +123,7 @@ function Page() {
                   type="text"
                   required
                   id="firstname"
-                  className="w-full bg-white text-primary border-2 border-gold rounded-lg p-3 text-lg focus:outline-none focus:border-opacity-50"
+                  className="w-full bg-white text-primary border-2 border-gold rounded-lg p-3 lg:text-xl text-[12px] focus:outline-none focus:border-opacity-50"
                   placeholder="Full Name"
                   value={firstName}
                   onChange={(e) => setFirstName(e.target.value)}
@@ -134,7 +134,7 @@ function Page() {
                   type="tel"
                   required
                   id="phone"
-                  className="w-full bg-white text-primary border-2 border-gold rounded-lg p-3 text-lg focus:outline-none focus:border-opacity-50"
+                  className="w-full bg-white text-primary border-2 border-gold rounded-lg p-3 lg:text-xl text-[12px] focus:outline-none focus:border-opacity-50"
                   placeholder="Phone Number"
                   value={phone}
                   onChange={(e) => setPhone(e.target.value)}
@@ -145,38 +145,38 @@ function Page() {
                   type="email"
                   required
                   id="email"
-                  className="w-full bg-white text-black border-2 border-gold rounded-lg p-3 text-md focus:outline-none focus:border-opacity-50"
+                  className="w-full bg-white text-black border-2 border-gold rounded-lg p-3 lg:text-xl text-[12px] focus:outline-none focus:border-opacity-50"
                   placeholder="Your Email"
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}
                 />
               </div>
               <div>
-                <h3 className="text-2xl font-bold mb-4">
+                <h3 className="md:text-2xl text-[16px] font-bold mb-4">
                   What skill(s) do you currently have?
                 </h3>
                 <div className="flex p-4 flex-wrap justify-center gap-4">
                   <button
                     onClick={() => handleDonationClick("Public Speaking")}
-                    className="bg-white text-primary hover:bg-opacity-80 py-3 px-6 rounded-lg text-md text-black font-semibold transition duration-300"
+                    className="bg-white text-primary hover:bg-opacity-80 py-3 px-6 rounded-lg lg:text-xl text-[12px] text-black font-semibold transition duration-300"
                   >
                     Public Speaking
                   </button>
                   <button
                     onClick={() => handleDonationClick("Social Media")}
-                    className="bg-white text-primary hover:bg-opacity-80 py-3 px-6 rounded-lg text-md text-black font-semibold transition duration-300"
+                    className="bg-white text-primary hover:bg-opacity-80 py-3 px-6 rounded-lg text-md lg:text-xl text-[12px] text-black font-semibold transition duration-300"
                   >
                     Social Media
                   </button>
                   <button
                     onClick={() => handleDonationClick("Writing")}
-                    className="bg-white text-primary hover:bg-opacity-80 py-3 px-6 rounded-lg text-md text-black font-semibold transition duration-300"
+                    className="bg-white text-primary hover:bg-opacity-80 py-3 px-6 rounded-lg text-md lg:text-xl text-[12px] text-black font-semibold transition duration-300"
                   >
                     Writing
                   </button>
                   <button
                     onClick={() => handleDonationClick("Graphic Design")}
-                    className="bg-white text-primary hover:bg-opacity-80 py-3 px-6 rounded-lg text-md text-black font-semibold transition duration-300"
+                    className="bg-white text-primary hover:bg-opacity-80 py-3 px-6 rounded-lg text-md lg:text-xl text-[12px] text-black font-semibold transition duration-300"
                   >
                     Graphic Design
                   </button>
@@ -192,13 +192,13 @@ function Page() {
                 <input
                   type="text"
                   id="custom-amount"
-                  className="w-full bg-white text-primary text-black border-2 border-gold rounded-lg p-3 text-lg focus:outline-none focus:border-opacity-50"
+                  className="w-full bg-white text-primary text-black border-2 border-gold rounded-lg p-3 lg:text-xl text-[12px] focus:outline-none focus:border-opacity-50"
                   placeholder="Enter skill"
                   value={customSkill}
                   onChange={(e) => setCustomSkill(e.target.value)}
                 />
                 <div className="flex justify-center">
-                  <div className="bg-white w-[130px] mt-2 font-semibold rounded-xl hover:bg-black cursor-pointer hover:text-white p-3">
+                  <div className="bg-white w-[130px] mt-2 lg:text-2xl text-[12px] font-semibold rounded-xl hover:bg-black cursor-pointer hover:text-white p-3">
                     <button onClick={() => handleDonationClick(customSkill)}>
                       Add Skill
                     </button>
@@ -206,7 +206,9 @@ function Page() {
                 </div>
               </div>
               <div>
-                <h3 className="text-2xl font-bold mb-1">Selected Skills</h3>
+                <h3 className="lg:text-2xl text-[16px] font-bold mb-1">
+                  Selected Skills
+                </h3>
                 <div className="flex p-4 flex-wrap justify-center gap-4">
                   {skills.map((skill, index) => (
                     <div
@@ -225,31 +227,31 @@ function Page() {
                 </div>
               </div>
               <div>
-                <h3 className="text-2xl font-bold mb-4">
+                <h3 className="lg:text-2xl text-[16px] font-bold mb-4 px-2">
                   What Interest do you have?
                 </h3>
                 <div className="flex p-4 flex-wrap justify-center gap-4">
                   <button
                     onClick={() => handleInterestClick(" Women Empowerment")}
-                    className="bg-white text-primary hover:bg-opacity-80 py-3 px-6 rounded-lg text-md text-black font-semibold transition duration-300"
+                    className="bg-white text-primary hover:bg-opacity-80 py-3 px-6 rounded-lg lg:text-xl text-[12px] text-black font-semibold transition duration-300"
                   >
                     Women Empowerment
                   </button>
                   <button
                     onClick={() => handleInterestClick(" Gender Equality")}
-                    className="bg-white text-primary hover:bg-opacity-80 py-3 px-6 rounded-lg text-md text-black font-semibold transition duration-300"
+                    className="bg-white text-primary hover:bg-opacity-80 py-3 px-6 rounded-lg lg:text-xl text-[12px] text-black font-semibold transition duration-300"
                   >
                     Gender Equality
                   </button>
                   <button
                     onClick={() => handleInterestClick("Poverty Eradication")}
-                    className="bg-white text-primary hover:bg-opacity-80 py-3 px-6 rounded-lg text-md text-black font-semibold transition duration-300"
+                    className="bg-white text-primary hover:bg-opacity-80 py-3 px-6 rounded-lg lg:text-xl text-[12px] text-black font-semibold transition duration-300"
                   >
                     Poverty Eradication
                   </button>
                   <button
                     onClick={() => handleInterestClick("Education")}
-                    className="bg-white text-primary hover:bg-opacity-80 py-3 px-6 rounded-lg text-md text-black font-semibold transition duration-300"
+                    className="bg-white text-primary hover:bg-opacity-80 py-3 px-6 rounded-lg lg:text-xl text-[12px] text-black font-semibold transition duration-300"
                   >
                     Education
                   </button>
@@ -271,7 +273,7 @@ function Page() {
                   onChange={(e) => setCustomInterest(e.target.value)}
                 />
                 <div className="flex justify-center">
-                  <div className="bg-white w-[130px] mt-2 font-semibold rounded-xl hover:bg-black cursor-pointer hover:text-white p-3">
+                  <div className="bg-white w-[130px] mt-2 font-semibold rounded-xl lg:text-xl text-[12px] hover:bg-black cursor-pointer hover:text-white p-3">
                     <button onClick={() => handleInterestClick(customInterest)}>
                       Add Interest
                     </button>
@@ -279,12 +281,14 @@ function Page() {
                 </div>
               </div>
               <div>
-                <h3 className="text-2xl font-bold mb-1">Selected Interest</h3>
+                <h3 className="lg:text-2xl text-[16px] font-bold mb-1">
+                  Selected Interest
+                </h3>
                 <div className="flex p-4 flex-wrap justify-center gap-4">
                   {interests.map((interest, index) => (
                     <div
                       key={index}
-                      className="bg-primary text-black bg-white p-2  rounded-lg flex items-center"
+                      className="bg-primary text-black bg-white p-2 lg:text-xl text-[12px]  rounded-lg flex items-center"
                     >
                       {interest}
                       <button
@@ -298,34 +302,36 @@ function Page() {
                 </div>
               </div>
               <div>
-                <h3 className="text-2xl font-bold mb-4">Days Available</h3>
+                <h3 className="lg:text-2xl text-[16px] font-bold mb-4">
+                  Days Available
+                </h3>
                 <div className="flex flex-wrap p-4 justify-center gap-4">
                   <button
-                    className="bg-white text-primary hover:bg-opacity-80 py-3 px-6 rounded-lg text-md text-black font-semibold transition duration-300"
+                    className="bg-white text-primary hover:bg-opacity-80 py-3 px-6 rounded-lg lg:text-xl text-[12px] text-black font-semibold transition duration-300"
                     onClick={() => handleDayClick("Monday")}
                   >
                     Monday
                   </button>
                   <button
-                    className="bg-white text-primary hover:bg-opacity-80 py-3 px-6 rounded-lg text-md text-black font-semibold transition duration-300"
+                    className="bg-white text-primary hover:bg-opacity-80 py-3 px-6 rounded-lg lg:text-xl text-[12px] text-black font-semibold transition duration-300"
                     onClick={() => handleDayClick("Tuesday")}
                   >
                     Tuesday
                   </button>
                   <button
-                    className="bg-white text-primary hover:bg-opacity-80 py-3 px-6 rounded-lg text-md text-black font-semibold transition duration-300"
+                    className="bg-white text-primary hover:bg-opacity-80 py-3 px-6 rounded-lg lg:text-xl text-[12px] text-black font-semibold transition duration-300"
                     onClick={() => handleDayClick("Wednesday")}
                   >
                     Wednesday
                   </button>
                   <button
-                    className="bg-white text-primary hover:bg-opacity-80 py-3 px-6 rounded-lg text-md text-black font-semibold transition duration-300"
+                    className="bg-white text-primary hover:bg-opacity-80 py-3 px-6 rounded-lg lg:text-xl text-[12px] text-black font-semibold transition duration-300"
                     onClick={() => handleDayClick(" Thursday")}
                   >
                     Thursday
                   </button>
                   <button
-                    className="bg-white text-primary hover:bg-opacity-80 py-3 px-6 rounded-lg text-md text-black font-semibold transition duration-300"
+                    className="bg-white text-primary hover:bg-opacity-80 py-3 px-6 rounded-lg lg:text-xl text-[12px] text-black font-semibold transition duration-300"
                     onClick={() => handleDayClick("Friday")}
                   >
                     Friday
@@ -333,12 +339,14 @@ function Page() {
                 </div>
               </div>
               <div>
-                <h3 className="text-2xl font-bold mb-4">Selected Days</h3>
+                <h3 className="lg:text-2xl text-[16px] font-bold mb-4">
+                  Selected Days
+                </h3>
                 <div className="flex p-4 justify-center gap-4">
                   {daysAvailable.map((day, index) => (
                     <div
                       key={index}
-                      className=" text-black bg-white p-2  rounded-lg flex items-center"
+                      className="lg:text-xl text-[12px] text-black bg-white p-2  rounded-lg flex items-center"
                     >
                       {day}
                       <button
@@ -353,7 +361,7 @@ function Page() {
               </div>
 
               <div className="flex justify-center">
-                <div className="bg-white w-[130px] font-semibold mt-5 rounded-xl hover:bg-black cursor-pointer hover:text-white p-3">
+                <div className="bg-white w-[130px] lg:text-xl text-[12px] font-semibold mt-5 rounded-xl hover:bg-black cursor-pointer hover:text-white p-3">
                   <button onClick={handleSuccess}>Submit</button>
                 </div>
               </div>
